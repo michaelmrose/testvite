633282972e6eadcd3c5fc9933be243db942c0f43
Michael Rose <michael@rosenetwork.net>
2023-09-07 21:20:07 -0700

    added typescript

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,26 @@
-var path = require('path');
-var express = require('express');
-var router = require('./lib/router');
-var _a = process.env.PORT,
-  PORT = _a === void 0 ? 3001 : _a;
-var app = express();
+const path = require('path');
+const express = require('express');
+const router = require('./lib/router');
+
+const { PORT = 3001 } = process.env;
+
+const app = express();
+
 // Middleware that parses json and looks at requests where the Content-Type header matches the type option.
 app.use(express.json());
+
 // Serve API requests from the router
 app.use('/api', router);
+
 // Serve app production bundle
 app.use(express.static('dist/app'));
+
 // Handle client routing, return all requests to the app
-app.get('*', function (_req, res) {
+app.get('*', (_req: any, res: any) => {
   res.sendFile(path.join(__dirname, 'app/index.html'));
 });
+
 console.log('hello from typescript');
-app.listen(PORT, function () {
-  console.log('Server listening at http://localhost:'.concat(PORT));
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
